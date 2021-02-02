1eed626969bff78dc78b4c6159952281460521ec
osbe0012 <chris.osberg@gmail.com>
2021-02-02 16:04:20 -0600

    Initial commit of project front end and back end

---
+++ spring/RestaurantStagingProject/src/test/java/com/revature/driver/RestaurantStagingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantStagingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
